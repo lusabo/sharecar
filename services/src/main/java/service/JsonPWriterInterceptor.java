3b94412958d987ed22a50d22cf58fc266571ee2b
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-02-25 17:30:58 -0300

    Limpeza no código para isolar o serviço.

---
+++ services/src/main/java/service/JsonPWriterInterceptor.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.io.IOException;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.annotations.interception.ServerInterceptor;
+import org.jboss.resteasy.spi.HttpRequest;
+import org.jboss.resteasy.spi.interception.MessageBodyWriterContext;
+import org.jboss.resteasy.spi.interception.MessageBodyWriterInterceptor;
+
+@Provider
+@ServerInterceptor
+public class JsonPWriterInterceptor implements MessageBodyWriterInterceptor {
+
+	@Context
+	private HttpRequest request;
+
+	@Override
+	public void write(MessageBodyWriterContext context) throws IOException, WebApplicationException {
+		String callback = request.getUri().getQueryParameters().getFirst("callback");
+
+		if (callback != null) {
+			context.getOutputStream().write(callback.getBytes());
+			context.getOutputStream().write("(".getBytes());
+			context.proceed();
+			context.getOutputStream().write(")".getBytes());
+		} else {
+			context.proceed();
+		}
+	}
+}
