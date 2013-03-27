83085e0195e1df0998ea97ddf3faa8fe234a32cc
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2013-03-27 18:47:39 -0300

    Implementanta a multi-autenticação onde as telas exigem autenticação via FORM e os serviços via BASIC

--- services/src/main/java/business/UserBC.java
+++ services/src/main/java/business/UserBC.java
@@ -1,9 +1,13 @@
 package business;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import persistence.UserDAO;
+import br.gov.frameworkdemoiselle.security.AfterLoginSuccessful;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.util.Beans;
 import entity.User;
 
 @BusinessController
@@ -11,17 +15,31 @@ public class UserBC {
 
 	@Inject
 	private UserDAO userDAO;
-	
+
 	public void insert(User user) throws Exception {
 		userDAO.insert(user);
 	}
-	
+
 	public void update(User user) throws Exception {
 		userDAO.update(user);
 	}
 
 	public User load(String username) throws Exception {
 		return userDAO.loadByUsername(username);
-	}	
-	
+	}
+
+	protected void afterLoginSuccessful(@Observes AfterLoginSuccessful event) throws Exception {
+		SecurityContext securityContext = Beans.getReference(SecurityContext.class);
+		
+		User currentUser = (User) securityContext.getCurrentUser();
+		User persistedUser = load(currentUser.getName());
+
+		if (persistedUser == null) {
+			insert(currentUser);
+
+		} else if (!currentUser.equals(persistedUser)) {
+			currentUser.setId(persistedUser.getId());
+			update(currentUser);
+		}
+	}
 }
